beae9e8b1ff0fd31ddfe806b8a7f87f47e0a5987
winstonzhengca <32969374+winstonzhengca@users.noreply.github.com>
2017-10-31 15:29:44 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<script src="/js/jquery-3.2.1.min.js"></script>
+		<script src="/js/blackjack.js"></script>
+		
+	</head>
+	<body>
+		<div>
+			<p id="display"></p>
+		</div>
+		<div>
+			<p id="dealer"></p>
+		</div>
+		<div>
+			<p id="result"></p>
+		</div>
+	</body>
+</html>
